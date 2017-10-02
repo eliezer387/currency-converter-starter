3dfd77ba5e615a9451228ea249e115d422bc132b
Eliezer Pe#a <eliezer.pena@sigis.com.ve>
2017-10-02 16:56:32 -0400

    List Item

--- app/components/Text/LastConverted.js
+++ app/components/Text/LastConverted.js
@@ -6,16 +6,16 @@ import moment from 'moment';
 import styles from './styles';
 
 const LastConverted = ({ base, quote, conversionRate, date }) => (
-    <Text style={styles.smallText}>
+  <Text style={styles.smallText}>
         1 {base} = {conversionRate} {quote} as of {moment(date).format('MMMM D, YYYY')}
-    </Text>
+  </Text>
 );
 
 LastConverted.propTypes = {
-    date: PropTypes.object,
-    base: PropTypes.string,
-    quote: PropTypes.string,
-    conversionRate: PropTypes.number,
+  date: PropTypes.object,
+  base: PropTypes.string,
+  quote: PropTypes.string,
+  conversionRate: PropTypes.number,
 };
 
 export default LastConverted;
--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+// import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4F607A',
@@ -10,6 +11,9 @@ EStyleSheet.build({
   $border: '#E2E2E2',
   $inputText: '#797979',
   $lightGrey: '#F0F0F0',
+  $darkText: '#343434',
+
+  outline: 0,
 });
 
-export default () => <Home />;
+export default () => <CurrencyList />;
